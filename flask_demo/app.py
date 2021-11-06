ffba63730e29f1b87a6d777c9d1be0651bbde5ee
noah <wigginno@oregonstate.edu>
2021-11-06 16:58:32 -0400

    replaced shiny demo with flask demo

---
+++ flask_demo/app.py
@@ -0,0 +1,67 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort, \
+    send_from_directory
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+# TODO: Only allow 1 file per upload box
+# TODO: Store uploaded files separately for each user session
+# TODO: Validate uploaded files
+
+def validate_counts(stream):
+    # Stub function for demo
+    return '.tsv'
+
+def validate_coldata(stream):
+    # Stub function for demo
+    return '.tsv'
+
+def validate_filter(stream):
+    # Stub function for demo
+    return '.txt'
+
+@app.route('/')
+def index():
+    files = os.listdir('uploads')
+    return render_template('uploadfiles.html', files=files)
+
+@app.route('/uploadcounts', methods=['POST'])
+def upload_counts():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext != '.tsv' or \
+                file_ext != validate_counts(uploaded_file.stream):
+            return "Invalid file extension", 400
+        uploaded_file.save('uploads/counts.tsv')
+    return redirect(url_for('index'))
+
+@app.route('/uploadcoldata', methods=['POST'])
+def upload_coldata():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext != '.tsv' or \
+                file_ext != validate_coldata(uploaded_file.stream):
+            return "Invalid file extension", 400
+        uploaded_file.save('uploads/coldata.tsv')
+    return redirect(url_for('index'))
+
+@app.route('/uploadfilter', methods=['POST'])
+def upload_filter():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext != '.txt' or \
+                file_ext != validate_filter(uploaded_file.stream):
+            return "Invalid file extension", 400
+        uploaded_file.save('uploads/filter.txt')
+    return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory('uploads', filename)
