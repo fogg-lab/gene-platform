a6cc01861bd2677dae63ff868082f58949158651
bean <bean@pop-os.localdomain>
2022-01-24 22:53:23 -0500

    added parameters and submit button

--- webapp/app.py
+++ webapp/app.py
@@ -4,6 +4,8 @@ import shutil
 import csv
 from datetime import timedelta
 import tempfile
+from tkinter import N
+from tkinter.messagebox import NO
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request, redirect, url_for, \
     send_from_directory, session
@@ -16,6 +18,9 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 app.config['DROPZONE_TIMEOUT'] = 120000 # timeout for uploads in milliseconds
 Session(app)
+
+RNA_SEQ_SCRIPT_LOCATION = "dge_analysis/rna_seq_dgea.R"
+MICROARRAY_SCRIPT_LOCATION = "dge_analysis/microarray_dgea.R"
 
 # backlog (in order)
 # - limit upload fields to one file (Noah) - done (a new upload overwrites old)
@@ -34,7 +39,7 @@ Session(app)
 @app.route('/')
 def index():
     files = os.listdir('user_files')
-    return render_template('uploadfiles.html', files=files)
+    return render_template('input_form.html', files=files)
 
 @app.route('/uploadcounts', methods=['POST'])
 def upload_counts():
@@ -75,14 +80,53 @@ def upload_config():
     filename = secure_filename(uploaded_file.filename)
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
-        if file_ext != '.txt':
+        if file_ext != '.txt' or file_ext != '.yml':
             return "Invalid file extension", 400
-        save_temp_file(uploaded_file, 'config.txt')
+        save_temp_file(uploaded_file, 'config.yml')
     return redirect(url_for('index'))
 
 @app.route('/user_files/<filename>')
 def upload(filename):
     return send_from_directory('user_files', filename)
+
+# submit()
+# submits input for analysis
+# generates the config file if the parameters are filled out
+@app.route('/submit', methods=['POST'])
+def submit():
+    # get whether analysis is microarray or RNA-Seq
+    data_type = request.args.get('data_type')
+    
+    # get config parameters
+    min_expr = request.args.get('min_expr')
+    min_prop = request.args.get('min_prop')
+    padj_thresh = request.args.get('padj_thresh')
+    adj_method = request.args.get('adj_method')
+    condition = request.args.get('condition')
+    contrast_level = request.args.get('contrast_level')
+    reference_level = request.args.get('reference_level')
+    use_qual_weights = request.args.get('use_qual_weights')
+
+    # if the parameters are set, generate config
+    if min_expr is not None and min_prop is not None \
+        and padj_thresh is not None and adj_method is not None \
+        and adj_method is not None and condition is not None \
+        and contrast_level is not None and reference_level is not None \
+        and use_qual_weights is not None:
+        generate_config(min_expr, min_prop, padj_thresh, adj_method, condition, \
+            contrast_level, reference_level, use_qual_weights)
+
+    # input files should be in the user directory by now
+    # TODO: validate input files before calling analysis
+
+    if data_type == "microarray":
+        subprocess.Popen(['MICROARRAY_SCRIPT_LOCATION %s' \
+            %("webapp/" + session["user_session_dir"])], shell=True)
+    elif data_type == "RNA_Seq":
+        subprocess.Popen(['RNA_SEQ_SCRIPT_LOCATION %s' \
+            %("webapp/" + session["user_session_dir"])], shell=True)
+
+    return
 
 @app.route('/display')
 def display_output():
@@ -109,7 +153,7 @@ def save_temp_file(file, filename):
     user_file = open(user_file_path, 'w')
 
     for line in file.readlines():
-        user_file.write(line.decode("utf-8") + '\n')
+        user_file.write(line.decode("utf-8"))
 
     user_file.close()
 
@@ -133,5 +177,20 @@ def cleanup_old_sessions():
 
             if age_seconds > 3600:
                 shutil.rmtree(old_dir)
+
+def generate_config(min_expr, min_prop, padj_thresh, adj_method, condition, \
+        contrast_level, reference_level, use_qual_weights):
+    config_file_path = session["user_session_dir"] + "/config.yml"
+    config_file = open(config_file_path, 'w')
+
+    config_file.write("min_expr:", str(min_expr))
+    config_file.write("min_prop:", str(min_prop))
+    config_file.write("padj_thresh:", str(padj_thresh))
+    config_file.write("adj_method:", str(adj_method))
+    config_file.write("condition:", str(condition))
+    config_file.write("contrast_level:", str(contrast_level))
+    config_file.write("reference_level:", str(reference_level))
+    config_file.write("use_qual_weights:", str(use_qual_weights))
+
 
 cleanup_old_sessions()
