6b4c9b31dc466f6f6aa2bbdda840ba1ec274165a
AiVu <nhanai950@gmail.com>
2022-03-07 09:01:10 -0800

    add validation for yml

--- validation/validateInput.py
+++ validation/validateInput.py
@@ -6,16 +6,18 @@
 # does not mean file is invalid, but it might be due to the FileValidation needs to wait for the other file (either
 # coldata or count data of the same analysis) to complete its task.
 
-
 import pandas as pd
 import numpy as np
 import os
+from cerberus import Validator
+import yaml
 
 UPLOAD_FOLDER = r"C:\Users\trungn\PycharmProjects\DGEAP\validation"
 
 
 class FileSystem:
     """holds valid formats of col_data, counts, and filter gene file"""
+
     def __init__(self):
         self.micro_array_col_data_dict = {'sample_name': "string", 'series': "string", 'title': "string",
                                           'condition': "string", 'endometriosis_stage': "string",
@@ -34,6 +36,67 @@ class FileSystem:
         self._rna_seq_col_file = {'type': '.tsv', 'format': self.rna_seq_col_dict}
 
         self._rna_seq_count_file = {'type': '.tsv', 'format': self.rna_seq_count_data_dict}
+        self._microarray_scheme = {
+            'min_expr': {
+                'type': 'integer',
+                'required': True
+            },
+            'min_prop': {
+                'type': 'float', 'min': 0.0, 'max': 1.0,
+                'required': True
+            },
+            'adj_method': {
+                'type': 'string', 'allowed': ['BH'],
+                'required': True
+            },
+            'condition': {
+                'type': 'string', 'allowed': ['condition'],
+                'required': True
+            },
+            'use_weight': {
+                'type': 'boolean',
+                'required': True
+            },
+            'contrast_level': {
+                'type': 'string', 'allowed': ['endometriosis', 'normal'],
+                'required': True
+            },
+            'reference_level': {
+                'type': 'string', 'allowed': ['endometriosis', 'normal'],
+                'required': True
+            },
+        }
+
+        self._rna_seq_schema = {
+            'min_expr': {
+                'type': 'integer',
+                'required': True
+            },
+            'min_prop': {
+                'type': 'float', 'min': 0.0, 'max': 1.0,
+                'required': True
+            },
+            'padj_thresh': {
+                'type': 'float', 'min': 0.0, 'max': 1.0,
+                'required': True
+            },
+            'adj_method': {
+                'type': 'string', 'allowed': ['BH'],
+                'required': True
+            },
+            'condition': {
+                'type': 'string', 'allowed': ['condition'],
+                'required': True
+            },
+            'contrast_level': {
+                'type': 'string', 'allowed': ['tumor', 'healthy'],
+                'required': True
+            },
+            'reference_level': {
+                'type': 'string', 'allowed': ['tumor', 'healthy'],
+                'required': True
+            }
+        }
 
     @staticmethod
     def merge_dicts(dict_1, final_dict):
@@ -60,9 +123,18 @@ class FileSystem:
         """get the valid format of the filter gene file"""
         return self._filter_gene_file
 
+    def get_config_schema_microarray(self):
+        """get the valid format of the config file for microarray's parameters"""
+        return self._microarray_scheme
+
+    def get_config_schema_rna_seq(self):
+        """get the valid format of the config file for microarray's parameters"""
+        return self._rna_seq_schema
+
 
 class FileValidation:
     """has the FileSystem and methods to validate input files: col-data, count data, and filter gene files"""
+
     def __init__(self):
 
         self._validate_status = False  # assume that the file has invalid format
@@ -70,7 +142,7 @@ class FileValidation:
         self._has_validated_rna_seq_col_file = False  # # holds the validated rna_seq filename
         self.file_obj = FileSystem()
         self._filename = ['filter gene', 'rna seguence coldata', 'rna sequence_count',
-                          'microarray coldata', 'microarray_count']
+                          'microarray coldata', 'microarray_count', 'config_microarray']
 
     def set_validation(self, state):
         """set the new validation status for a file"""
@@ -103,10 +175,42 @@ class FileValidation:
     @staticmethod
     def convert_string_to_dict(column_list, data_type):
         """convert a list into dictionary with values be data type"""
-        dict_1 = {}
+        string_data_type_dict = {}
         for gene in column_list:
-            dict_1[gene] = data_type
-        return dict_1
+            string_data_type_dict[gene] = data_type
+        return string_data_type_dict
+
+    def validate_config(self, config_name):
+        file_path = os.path.join(UPLOAD_FOLDER, config_name)
+        try:
+            with open(file_path, 'r') as stream:
+                try:
+                    config = yaml.safe_load(stream)
+                    if config_name.endswith('microarray.yml'):
+                        v = Validator(self.file_obj.get_config_schema_microarray())
+                        result = v.validate(config, self.file_obj.get_config_schema_microarray())
+                        if result is True:
+                            self.set_validation(True)
+                        else:
+                            self.set_validation(False)
+                            print(v.errors)
+
+                    if config_name.endswith('rna_seq.yml'):
+                        v = Validator(self.file_obj.get_config_schema_rna_seq())
+                        result = v.validate(config, self.file_obj.get_config_schema_rna_seq())
+                        if result is True:
+                            self.set_validation(True)
+                        else:
+                            self.set_validation(False)
+                            print(v.errors)
+                except yaml.YAMLError as exception:
+                    raise exception
+
+        except IOError:
+            fatal_error("Could not open the configuration file name {}".format(config_name))
+
+        finally:
+            return self.get_validation()
 
     def validate_file(self, filename):
         """validate the following files: col, count, and filter gene. Returns true
@@ -170,44 +274,50 @@ class FileValidation:
             return self.get_validation()
 
 
-def main():
-    valid = FileValidation()
-    file_name_1 = "filter_gene.txt"
-    print(file_name_1)
-    print(valid.validate_file(file_name_1))
-
-    print("2= expect pend=========================")
-    file_name_4 = 'microarray_endometriosis_counts.tsv'
-    print(file_name_4)
-    print(valid.validate_file(file_name_4))
-
-    print("3= expect pend=========================")
-    file_name_5 = 'rna_seq_ucec_counts.tsv'
-    print(file_name_5)
-    print(valid.validate_file(file_name_5))
-
-    print("4=true=========================")
-    file_name_2 = 'microarray_endometriosis_coldata.tsv'
-    print(file_name_2)
-    print(valid.validate_file(file_name_2))
-
-    print("5=true=========================")
-    file_name_4 = 'microarray_endometriosis_counts.tsv'
-    print(file_name_4)
-    print(valid.validate_file(file_name_4))
-    print("==========================")
-
-    print("6=true=========================")
-    file_name_3 = 'rna_seq_ucec_coldata.tsv'
-    print(file_name_3)
-    print(valid.validate_file(file_name_3))
-
-    print("7=true=========================")
-    file_name_5 = 'rna_seq_ucec_counts.tsv'
-    print(file_name_5)
-    print(valid.validate_file(file_name_5))
-
-
-if __name__ == '__main__':
-    main()
-
+# def main():
+#     # this is to show examples of how to use the program to check for file validation
+#     valid = FileValidation()
+#     config1 = 'config_microarray.yml'
+#     print(valid.validate_config(config1))
+#
+#     config2 = 'config_rna_seq.yml'
+#     print(valid.validate_config(config2))
+#
+#     file_name_1 = "filter_gene.txt"
+#     print(file_name_1)
+#     print(valid.validate_file(file_name_1))
+#
+#     print("2= expect pend=========================")
+#     file_name_4 = 'microarray_endometriosis_counts.tsv'
+#     print(file_name_4)
+#     print(valid.validate_file(file_name_4))
+#
+#     print("3= expect pend=========================")
+#     file_name_5 = 'rna_seq_ucec_counts.tsv'
+#     print(file_name_5)
+#     print(valid.validate_file(file_name_5))
+#
+#     print("4=true=========================")
+#     file_name_2 = 'microarray_endometriosis_coldata.tsv'
+#     print(file_name_2)
+#     print(valid.validate_file(file_name_2))
+#
+#     print("6=true=========================")
+#     file_name_3 = 'rna_seq_ucec_coldata.tsv'
+#     print(file_name_3)
+#     print(valid.validate_file(file_name_3))
+#
+#     # print("5=true=========================")
+#     # file_name_4 = 'microarray_endometriosis_counts.tsv'
+#     # print(file_name_4)
+#     # print(valid.validate_file(file_name_4))
+#     # print("==========================")
+#
+#     # print("7=true=========================")
+#     # file_name_5 = 'rna_seq_ucec_counts.tsv'
+#     # print(file_name_5)
+#     # print(valid.validate_file(file_name_5))
+#
+#
+# if __name__ == '__main__':
+#     main()
