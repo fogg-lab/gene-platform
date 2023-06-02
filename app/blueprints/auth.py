5d677e340e6b362e1383e3e325725f96b0a7a3a3
Noah Wiggin <wigginno@oregonstate.edu>
2023-06-02 19:09:17 -0400

    fix conditional bug

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -32,16 +32,16 @@ def before_request():
     if not auth_bp.initialized:
         init_env()
     enable_google_auth = os.getenv("ENABLE_GOOGLE_AUTH")
+    is_login_endpoint = request.endpoint in ["auth_bp.login", "auth_bp.callback"]
     if enable_google_auth and enable_google_auth.lower() not in ["false", "0"]:
-        is_login_endpoint = request.endpoint in ["auth_bp.login", "auth_bp.callback"]
         if not current_user.is_authenticated and not is_login_endpoint:
             return redirect("/login")
-        if not is_login_endpoint:
-            # Log the user in as a guest
-            if not User.get("guest1"):
-                User.create("guest1", "John Doe", "email@domain.com")
-            user = User.get("guest1")
-            login_user(user)
+    if not is_login_endpoint:
+        # Log the user in as a guest
+        if not User.get("guest1"):
+            User.create("guest1", "John Doe", "email@domain.com")
+        user = User.get("guest1")
+        login_user(user)
 
 
 @auth_bp.route("/login")
