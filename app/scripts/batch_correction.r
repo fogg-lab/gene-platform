fbb036045b45bcd9fbdd75bf9645c05cba53e181
Noah Wiggin <77313083+wigginno@users.noreply.github.com>
2023-08-16 14:34:30 -0400

    Improve variable names in batch_correction.r and remove tidyverse import

--- app/scripts/batch_correction.r
+++ app/scripts/batch_correction.r
@@ -1,36 +1,37 @@
-suppressMessages(suppressWarnings(library(tidyverse)))
+suppressMessages(suppressWarnings(library(readr)))
 suppressMessages(suppressWarnings(library(sva)))
+suppressMessages(suppressWarnings(library(dplyr)))
 
 args = commandArgs(trailingOnly = TRUE)
 
 # Paths
-countspath = args[1]
-coldatapath = args[2]
+expression_file = args[1]
+clinical_file = args[2]
 output_dir = args[3]
 data_type = args[4]
 
-# Read in the sample data and counts
-counts <- readr::read_tsv(countspath)
-metadata <- readr::read_tsv(coldatapath)
+# Read in the sample data and expression
+expression <- read_tsv(expression_file)
+metadata <- read_tsv(clinical_file)
 
 # Prep batch correction
-rma_expr <- as.matrix(counts[-1])
-rownames(rma_expr) <- counts$symbol
+expression_matrix <- as.matrix(expression[-1])
+rownames(expression_matrix) <- expression$symbol
 model_m <- model.matrix(~ condition, data = metadata)
 batch <- metadata$batch
 
 # Get symbol column for adding to output later
-symbol <- counts$symbol
+symbol <- expression$symbol
 symbols <- data.frame(symbol)
 
 # Batch correction
 if (data_type == "rnaseq") {
-  bc_rma_expr <- ComBat_seq(rma_expr, batch = batch, group = NULL, covar_mod=model_m)
+  bc_expression_matrix <- ComBat_seq(expression_matrix, batch = batch, group = NULL, covar_mod=model_m)
 } else { # microarray
-  bc_rma_expr <- ComBat(rma_expr, batch = batch, mod = model_m, ref.batch = 1)
+  bc_expression_matrix <- ComBat(expression_matrix, batch = batch, mod = model_m, ref.batch = 1)
 }
 
 # Add symbol column and write the dataframe to a file
-bc_rma_expr <- data.frame(bc_rma_expr)
-result <- bind_cols(symbols, bc_rma_expr)
+bc_expression_df <- data.frame(bc_expression_matrix)
+result <- bind_cols(symbols, bc_expression_df)
 write_tsv(result, file.path(output_dir, "batch_corrected_counts.tsv"))
