5751114a8ff48336f82603581fbd814d7e0e4502
lscannon12 <lscannon12@gmail.com>
2024-07-04 06:46:28 -0700

    Fixed Plotly.js support

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -48,8 +49,19 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
+    fallback: {
+      "buffer": require.resolve('buffer/'),
+      "stream": require.resolve('stream-browserify'),
+      "process": require.resolve('process/browser')
+    }
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser'
+    })
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'public')
