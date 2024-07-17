adb5fdf1be2df7d03307bfe6047c3d9fbd974fc1
lscannon12 <lscannon12@gmail.com>
2024-07-17 10:40:02 -0700

    Changed homepage. Added functionality to analysis form.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import IconButton from '../components/ui/IconButton';
+import logo from '../assets/fogg_logo.svg';
 
 const Home = () => {
 	const navigate = useNavigate();
@@ -16,7 +17,8 @@ const Home = () => {
 
 	return (
 		<div id="homepage_main_div">
-			<h1>GENE Platform</h1>
+			{/* <h1>GENE Platform</h1> */}
+			<img src={logo} id="fogg_logo_home" alt="Fogg Labs Logo" />
 			<h2>Gene Expression Explorer</h2>
 			<div id="homepage_buttons">
 				<IconButton iconFilename="biotech.png" label="Start Analysis" onClick={handleAnalysisClick} />
