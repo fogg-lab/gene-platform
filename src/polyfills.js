e0fbfb38381c1cbf01431acd1e91469ee04c4c39
Noah Wiggin <wigginno@oregonstate.edu>
2024-08-16 01:49:29 -0400

    Updates

--- src/polyfills.js
+++
@@ -1,9 +0,0 @@
-import { Buffer } from 'buffer';
-import stream from 'stream-browserify';
-import process from 'process/browser';
-import assert from 'assert';
-
-global.Buffer = Buffer;
-global.process = process;
-global.stream = stream;
-global.assert = assert;
